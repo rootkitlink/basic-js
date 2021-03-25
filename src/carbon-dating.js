e2f4d20fb95203ea54d6449b2abcd0383714f48b
Sergei Goncharov <rootkitlink@gmail.com>
2021-03-25 10:12:21 +0200

    feat: did carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,14 +5,15 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
 
+  if (isNaN(sampleActivity)) return false;
   if (typeof sampleActivity === 'string' && +sampleActivity !== undefined){
-    if (+sampleActivity <= 0) return false;
-    let k = 0.693 / HALF_LIFE_PERIOD;
-  let t;
-  t = Math.log( MODERN_ACTIVITY / +sampleActivity) / k;
-  return Math.ceil(t);
-  }
-  else return false;
-  
-  
+    if (Number(sampleActivity) <= 0 || Number(sampleActivity) >= MODERN_ACTIVITY) return false;
+    else {
+      let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.log( MODERN_ACTIVITY / Number(sampleActivity)) / k;
+    return Math.ceil(t);
+    }
+    
+    }
+    else return false;
 };
