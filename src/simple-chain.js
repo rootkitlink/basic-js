df352758e991e84c7eed61b41de022b0a52b8690
Sergei Goncharov <rootkitlink@gmail.com>
2021-03-26 13:04:56 +0200

    feat: did simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
+  
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    value === undefined ? this.arr.push('()') : this.arr.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position === 'number'){
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
+    else {
+      this.arr = [];
+      throw new 'Error';
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let newArr = this.arr.join('~~');
+    this.arr = [];
+    return newArr;
   }
+  
 };
 
 module.exports = chainMaker;
